515b65e53d7d46e4869ced194cfb6fffdfc6155f
Carlos <cgumuciolabbe@gmail.com>
2023-11-25 22:24:48 +0800

    fix: fixed image slider size

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -7,13 +7,6 @@ interface ImageSliderProps {
   interval?: number;
 }
 
-const ImageSliderContainer = styled(Box)({
-  position: 'relative',
-  width: '100%',
-  height: '100%',
-  overflow: 'hidden',
-});
-
 const ImageSliderImage = styled(Box)({
   position: 'absolute',
   top: 0,
@@ -53,11 +46,27 @@ function ImageSlider({ interval = 5000 }: ImageSliderProps) {
   }, [currentImageIndex, interval]);
 
   return (
-    <ImageSliderContainer>
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: {
+          xs: '100vh',
+          sm: '95vh',
+          md: '85vh',
+          lg: '80vh',
+        },
+        overflow: 'hidden',
+      }}
+    >
       {images.map((image, index) => (
         <ImageSliderImage
           key={index}
-          style={{ backgroundImage: `url(${image})`, opacity: index === currentImageIndex ? 1 : 0 }}
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundPosition: 'center',
+            opacity: index === currentImageIndex ? 1 : 0,
+          }}
         >
           {index === currentImageIndex && <ImageSliderOverlay />}
         </ImageSliderImage>
@@ -96,7 +105,7 @@ function ImageSlider({ interval = 5000 }: ImageSliderProps) {
         </ImageSliderText>
         <SearchBar />
       </Box>
-    </ImageSliderContainer>
+    </Box>
   );
 }
 
