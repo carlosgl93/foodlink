d746a65786559a1dd30943d7dc4d610f1fc4a45b
Carlos <cgumuciolabbe@gmail.com>
2024-03-07 22:25:09 +0800

    fix: snackbar controller naming

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -1,12 +1,12 @@
 import { notificationState } from '@/store/snackbar';
-import { snackbarController } from '@/store/snackbar/useSnackbarController';
+import { useSnackbarController } from '@/store/snackbar/useSnackbarController';
 import { Alert, Snackbar } from '@mui/material';
 import { useRecoilState } from 'recoil';
 
 export const NotificationSnackbar = () => {
   const [notification] = useRecoilState(notificationState);
   const { open, message, severity } = notification;
-  const { onClose } = snackbarController();
+  const { onClose } = useSnackbarController();
 
   return (
     <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
